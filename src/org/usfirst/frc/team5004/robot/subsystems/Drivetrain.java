00df9db1e06208b1d37633aae7f88b22d1ac96be
Cole Carrow <colec518@gmail.com>
2016-02-16 12:45:13 -0800

    oops forgot a semicolon

--- src/org/usfirst/frc/team5004/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5004/robot/subsystems/Drivetrain.java
@@ -17,23 +17,28 @@ public class Drivetrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
+	//local variables for joysticks
 	Joystick joyLeft = Robot.oi.joyDriverLeft;
 	Joystick joyRight = Robot.oi.joyDriverRight;
 	
+	//initialize motor controllers from port numbers
 	CANTalon l1 = new CANTalon(RobotMap.leftMotor1);
 	CANTalon l2 = new CANTalon(RobotMap.leftMotor2);
 	CANTalon r1 = new CANTalon(RobotMap.rightMotor1);
 	CANTalon r2 = new CANTalon(RobotMap.rightMotor2);
 	
 	public void init(){
+		//tell motor l2 to follow l1
 		l2.changeControlMode(TalonControlMode.Follower);
 		l2.set(RobotMap.leftMotor1);
 		
+		//tell motor r2 to follow r1
 		r2.changeControlMode(TalonControlMode.Follower);
 		r2.set(RobotMap.rightMotor1);
 	}
 
 	public void tankDrive(){
+		//set motor speeds to joystick values
 		r1.set(joyLeft.getY());
 		r2.set(joyRight.getY());
 	}
